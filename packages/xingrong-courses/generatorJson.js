95150c45c05397c352acb02566ec20c675735987
tealyao <yaowang1@qq.com>
2024-06-02 23:13:21 +0800

    feat: add statements

--- packages/xingrong-courses/generatorJson.js
+++ packages/xingrong-courses/generatorJson.js
@@ -13,20 +13,26 @@ const lines = text.trim().split('\n');
 const data = [];
 
 // 遍历行的数组，每次取两行
-for (let i = 0; i < lines.length; i += 2) {
+for (let i = 0; i < lines.length; i += 5) {
     // 创建一个对象，其中 chinese 是第 i+1 行，english 是第 i 行，soundmark 是空字符串
-    const item = {
+    const item1 = {
         chinese: lines[i + 1],
-        english: lines[i + 0],
-        soundmark: ""
+        english: lines[i + 0].split(' ')[0],
+        soundmark: lines[i + 0].split(' ')[1],
     };
+    const item2 = {
+        chinese: lines[i + 3],
+        english: lines[i + 2].replace(/\.$/, ''),
+        soundmark: '',
+    };
+
     // 将对象添加到数组中
-    data.push(item);
+    data.push(item1, item2);
 }
 
 // 将数组转换为 JSON 格式
 const json_data = JSON.stringify(data, null, 4);
 
 // 写入 JSON 数据到文件
-const targetPath = path.resolve(__dirname, './data/courses/19.json');
+const targetPath = path.resolve(__dirname, './data/courses/31.json');
 fs.writeFileSync(targetPath, json_data);
